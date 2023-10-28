03637ba011c6c5d678106374109f2a3867d2e055
rayhan-s <shadowray1711@gmail.com>
2023-10-28 17:35:44 +0700

    FLOWERDATA

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -3,17 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:flower_store/controllers/controller.dart';
 
-class FlowerCard {
-  final String image;
-  final String name;
-  final String scientificName;
-  final String description;
-  final String price;
-
-  FlowerCard(
-      this.image, this.name, this.scientificName, this.description, this.price);
-}
-
 class HomeView extends StatelessWidget {
   final FlowerController homeController = Get.put(FlowerController());
 
@@ -25,6 +14,40 @@ class HomeView extends StatelessWidget {
       "Bunga daisy, atau yang juga dikenal sebagai bunga matahari...",
       "10k",
     ),
+    FlowerCard(
+      'assets/images/lily.png',
+      "Lili",
+      "Lilium",
+      "Bunga lily adalah jenis bunga yang terkenal dengan bentuk kelopaknya yang indah dan aroma harumnya yang khas. Mereka biasanya memiliki tangkai tinggi dengan bunga yang besar dan berbentuk lonceng atau terbuka seperti cangkir. Lily memiliki berbagai variasi warna, termasuk putih, merah, oranye, kuning, dan pink. Bunga ini sering dianggap sebagai simbol keindahan, kemurnian, dan kesuburan. Lily sering digunakan dalam pengaturan bunga potong, taman hias, dan sering kali menjadi elemen penting dalam banyak upacara dan perayaan, termasuk pernikahan dan pemakaman. Beberapa varietas lily juga dianggap beracun bagi hewan peliharaan, sehingga perlu ditempatkan dengan hati-hati di lingkungan di mana hewan dapat mengonsumsinya.",
+      "10k",
+    ),
+    FlowerCard(
+      'assets/images/orchid.png',
+      "Orchid",
+      "Orchidaceae",
+      "Anggrek,(Orchidaceae) merupakan salah satu keluarga tumbuhan berbunga yang paling beragam dan besar, dengan ribuan spesies yang tersebar di seluruh dunia. Mereka dikenal karena keindahan dan kerumitan bunga mereka. Anggrek memiliki adaptasi yang unik, termasuk beragam bentuk dan warna bunga, serta interaksi yang kompleks dengan hewan penyerbuk, seperti lebah dan kupu-kupu. Selain itu, anggrek memiliki peran penting dalam industri hortikultura dan perdagangan bunga potong. Mereka juga menjadi simbol cinta, keindahan, dan kemewahan dalam banyak budaya, menjadikannya salah satu keluarga tumbuhan paling dihargai di seluruh dunia.",
+      "10k",
+    ),
+    FlowerCard(
+      'assets/images/rose.png',
+      "Rose",
+      "Rosa multiflora L.",
+      "Bunga yang sangat terkenal dan sering dianggap sebagai simbol cinta dan keindahan. Bunga mawar dikenal dengan berbagai warna, termasuk merah, putih, kuning, pink, dan oranye. Setiap warna mawar memiliki arti simbolisnya sendiri; misalnya, mawar merah sering dikaitkan dengan cinta dan gairah. Mawar juga dikenal dengan aroma harumnya yang khas dan daunnya yang tajam. Mawar umumnya digunakan dalam pengaturan bunga potong, parfum, dan pengobatan tradisional. Mawar juga sering ditanam sebagai tanaman hias di taman. Mawar adalah salah satu bunga paling ikonik dan dicintai di seluruh dunia.",
+      "10k",
+    ),
+    FlowerCard(
+        'assets/images/sunflower.png',
+        "Sunflower",
+        "Helianthus A L",
+        "Bunga yang dikenal dengan ciri khasnya yaitu kepala bunga besar yang mengikuti pergerakan matahari. Bunga matahari memiliki tangkai tinggi dan kepala bunga yang besar, yang terdiri dari petal kuning cerah yang tersusun dalam spiral. Bunga ini adalah simbol kebahagiaan dan kehangatan, serta sering dikaitkan dengan matahari dan keceriaan. Bunga matahari juga memiliki biji besar yang biasanya diambil dan dimakan sebagai camilan atau dihasilkan sebagai minyak biji matahari. Mereka sering ditanam sebagai tanaman hias dan digunakan dalam pengaturan bunga potong. Bunga matahari adalah tanda penghormatan terhadap matahari dan sumber inspirasi bagi banyak seniman dan penyair.",
+        "10k"),
+    FlowerCard(
+      'assets/images/tulip.png',
+      "Daisy",
+      "Tulipa",
+      "bunga yang terkenal dengan kelopaknya yang berbentuk seperti cawan dan tersedia dalam berbagai warna yang cerah, seperti merah, kuning, oranye, pink, dan ungu. Tulip sering dikaitkan dengan simbol cinta dan keindahan. Mereka berasal dari Asia Tengah dan menjadi salah satu bunga hias paling populer di dunia. Bunga tulip sering dijadikan tanda kebahagiaan dan kesempurnaan, dan mereka sering kali menjadi bagian penting dalam acara pernikahan, perayaan musim semi, dan taman hias. Tulip juga memiliki beberapa varietas yang berbeda, termasuk tulip Darwin yang besar dan berwarna-warni, serta tulip lili dengan bentuk kelopak yang lebih panjang dan ramping.",
+      "10k",
+    )
   ];
 
   int _selectedIndex = 0;
@@ -139,7 +162,7 @@ class HomeView extends StatelessWidget {
     BottomNavBarItem(
       imagePath: 'assets/images/data.png',
       action: () {
-        () => Get.toNamed('/WebviewPage');
+        () => Get.toNamed('/addImage');
       },
     ),
     BottomNavBarItem(
