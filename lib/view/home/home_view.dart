3fae197be23decab3f6a99dccf09561220a68f69
rayhan-s <shadowray1711@gmail.com>
2023-10-28 17:19:45 +0700

    NAVBAR1

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:flower_store/model/model.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:flower_store/controllers/controller.dart';
@@ -15,12 +16,26 @@ class FlowerCard {
 
 class HomeView extends StatelessWidget {
   final FlowerController homeController = Get.put(FlowerController());
+
+  final List<FlowerCard> flowerCards = [
+    FlowerCard(
+      'assets/images/daisy.png',
+      "Daisy",
+      "Bellis perennis",
+      "Bunga daisy, atau yang juga dikenal sebagai bunga matahari...",
+      "10k",
+    ),
+    // Add more FlowerCard objects for other flowers...
+  ];
+
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade200,
       floatingActionButton: FloatingActionButton(
-        onPressed: () => Get.toNamed('/planList'),
+        onPressed: () => Get.toNamed('/addImage'),
         child: const Icon(Icons.add_rounded),
       ),
       body: Padding(
@@ -28,107 +43,13 @@ class HomeView extends StatelessWidget {
         child: SafeArea(
           child: Column(
             children: [
-              // Header...
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Icon(
-                    Icons.format_list_bulleted_rounded,
-                    color: Colors.black,
-                  ),
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.shopping_cart,
-                        color: Colors.black,
-                      ),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      Icon(
-                        Icons.search,
-                        color: Colors.black,
-                      ),
-                    ],
-                  )
-                ],
-              ),
-
-              const SizedBox(
-                height: 30,
-              ),
-
-              const Text(
-                "Welcome to Griya Flower",
-                style: TextStyle(
-                  fontSize: 25,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("Populer"),
-                  Text("Recomended"),
-                  Text("Indor"),
-                  Text("Outdor"),
-                ],
-              ),
-              const SizedBox(
-                height: 20,
-              ),
               Expanded(
                 child: SingleChildScrollView(
                   child: Column(
                     children: [
-                      cardBunga(
-                        FlowerCard(
-                          'assets/images/daisy.png',
-                          "Daisy",
-                          "Bellis perennis",
-                          "Bunga daisy, atau yang juga dikenal sebagai bunga matahari, adalah jenis bunga yang populer dan mudah dikenali karena memiliki kepala bunga berbentuk cakram dengan kelopak bunga berwarna putih dan pusat berwarna kuning. Bunga ini biasanya memiliki tangkai yang panjang dan daun berbentuk daun lanset. Bunga daisy sering dianggap sebagai simbol kesucian, kepolosan, dan kebahagiaan. Mereka biasanya tumbuh di berbagai kondisi cuaca dan tanah, membuatnya cocok untuk taman dan pot bunga. Daisy juga dapat digunakan dalam pengaturan bunga segar atau dijadikan hiasan dalam berbagai perayaan dan acara.",
-                          "10k",
-                        ),
-                      ),
-                      cardBunga(FlowerCard(
-                        'assets/images/lily.png',
-                        "Lili",
-                        "Lilium",
-                        "Bunga lily adalah jenis bunga yang terkenal dengan bentuk kelopaknya yang indah dan aroma harumnya yang khas. Mereka biasanya memiliki tangkai tinggi dengan bunga yang besar dan berbentuk lonceng atau terbuka seperti cangkir. Lily memiliki berbagai variasi warna, termasuk putih, merah, oranye, kuning, dan pink. Bunga ini sering dianggap sebagai simbol keindahan, kemurnian, dan kesuburan. Lily sering digunakan dalam pengaturan bunga potong, taman hias, dan sering kali menjadi elemen penting dalam banyak upacara dan perayaan, termasuk pernikahan dan pemakaman. Beberapa varietas lily juga dianggap beracun bagi hewan peliharaan, sehingga perlu ditempatkan dengan hati-hati di lingkungan di mana hewan dapat mengonsumsinya.",
-                        "10k",
-                      )),
-                      cardBunga(FlowerCard(
-                        'assets/images/orchid.png',
-                        "Orchid",
-                        "Orchidaceae",
-                        "Anggrek,(Orchidaceae) merupakan salah satu keluarga tumbuhan berbunga yang paling beragam dan besar, dengan ribuan spesies yang tersebar di seluruh dunia. Mereka dikenal karena keindahan dan kerumitan bunga mereka. Anggrek memiliki adaptasi yang unik, termasuk beragam bentuk dan warna bunga, serta interaksi yang kompleks dengan hewan penyerbuk, seperti lebah dan kupu-kupu. Selain itu, anggrek memiliki peran penting dalam industri hortikultura dan perdagangan bunga potong. Mereka juga menjadi simbol cinta, keindahan, dan kemewahan dalam banyak budaya, menjadikannya salah satu keluarga tumbuhan paling dihargai di seluruh dunia.",
-                        "10k",
-                      )),
-                      cardBunga(FlowerCard(
-                        'assets/images/rose.png',
-                        "Rose",
-                        "Rosa multiflora L.",
-                        "Bunga yang sangat terkenal dan sering dianggap sebagai simbol cinta dan keindahan. Bunga mawar dikenal dengan berbagai warna, termasuk merah, putih, kuning, pink, dan oranye. Setiap warna mawar memiliki arti simbolisnya sendiri; misalnya, mawar merah sering dikaitkan dengan cinta dan gairah. Mawar juga dikenal dengan aroma harumnya yang khas dan daunnya yang tajam. Mawar umumnya digunakan dalam pengaturan bunga potong, parfum, dan pengobatan tradisional. Mawar juga sering ditanam sebagai tanaman hias di taman. Mawar adalah salah satu bunga paling ikonik dan dicintai di seluruh dunia.",
-                        "10k",
-                      )),
-                      cardBunga(FlowerCard(
-                        'assets/images/sunflower.png',
-                        "Sunflower",
-                        "Helianthus A L",
-                        "Bunga yang dikenal dengan ciri khasnya yaitu kepala bunga besar yang mengikuti pergerakan matahari. Bunga matahari memiliki tangkai tinggi dan kepala bunga yang besar, yang terdiri dari petal kuning cerah yang tersusun dalam spiral. Bunga ini adalah simbol kebahagiaan dan kehangatan, serta sering dikaitkan dengan matahari dan keceriaan. Bunga matahari juga memiliki biji besar yang biasanya diambil dan dimakan sebagai camilan atau dihasilkan sebagai minyak biji matahari. Mereka sering ditanam sebagai tanaman hias dan digunakan dalam pengaturan bunga potong. Bunga matahari adalah tanda penghormatan terhadap matahari dan sumber inspirasi bagi banyak seniman dan penyair.",
-                        "10k",
-                      )),
-                      cardBunga(FlowerCard(
-                        'assets/images/tulip.png',
-                        "Daisy",
-                        "Tulipa",
-                        "bunga yang terkenal dengan kelopaknya yang berbentuk seperti cawan dan tersedia dalam berbagai warna yang cerah, seperti merah, kuning, oranye, pink, dan ungu. Tulip sering dikaitkan dengan simbol cinta dan keindahan. Mereka berasal dari Asia Tengah dan menjadi salah satu bunga hias paling populer di dunia. Bunga tulip sering dijadikan tanda kebahagiaan dan kesempurnaan, dan mereka sering kali menjadi bagian penting dalam acara pernikahan, perayaan musim semi, dan taman hias. Tulip juga memiliki beberapa varietas yang berbeda, termasuk tulip Darwin yang besar dan berwarna-warni, serta tulip lili dengan bentuk kelopak yang lebih panjang dan ramping.",
-                        "10k",
-                      )),
+                      ...flowerCards
+                          .map((flowerCard) => cardBunga(flowerCard))
+                          .toList(),
                       const SizedBox(
                         height: 50,
                       ),
@@ -140,8 +61,89 @@ class HomeView extends StatelessWidget {
           ),
         ),
       ),
+      // Create a custom bottom navigation bar with clickable images.
+      bottomNavigationBar: Container(
+        height: 60, // Adjust the height as needed
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey,
+              offset: Offset(0, -2),
+              blurRadius: 6,
+            ),
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: List.generate(
+            _bottomNavBarItems.length,
+            (index) => buildBottomNavItem(index),
+          ),
+        ),
+      ),
     );
   }
+
+  // Define the list of bottom navigation bar items without 'const'.
+  static List<BottomNavBarItem> _bottomNavBarItems = [
+    BottomNavBarItem(
+      imagePath: 'assets/home.png',
+      action: () {
+        // Define the action for the "Home" button
+      },
+    ),
+    BottomNavBarItem(
+      imagePath: 'assets/search.png',
+      action: () {
+        // Define the action for the "Search" button
+      },
+    ),
+    BottomNavBarItem(
+      imagePath: 'assets/favorite.png',
+      action: () {
+        // Define the action for the "Favorite" button
+      },
+    ),
+    BottomNavBarItem(
+      imagePath: 'assets/cart.png',
+      action: () {
+        // Define the action for the "Cart" button
+      },
+    ),
+    BottomNavBarItem(
+      imagePath: 'assets/profile.png',
+      action: () {
+        // Define the action for the "Profile" button
+      },
+    ),
+  ];
+
+  // Build a clickable image widget for each navigation item.
+  Widget buildBottomNavItem(int index) {
+    return InkWell(
+      onTap: () {
+        // Execute the action associated with the button.
+        _bottomNavBarItems[index].action();
+        // Update the selected tab.
+        setState(() {
+          _selectedIndex = index;
+        });
+      },
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Image.asset(
+          _bottomNavBarItems[index].imagePath,
+          width: 30,
+          height: 30,
+          color: _selectedIndex == index ? Colors.blue : Colors.grey,
+        ),
+      ),
+    );
+  }
+
+  // Define the setState method to update the selected tab.
+  void setState(VoidCallback fn) {}
 }
 
 Widget cardBunga(FlowerCard flowerCard) {
