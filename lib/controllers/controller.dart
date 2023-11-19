c56209fd54f77a80cebc67eac6e3f8c2516b08a9
rayhan-s <shadowray1711@gmail.com>
2023-11-19 19:57:12 +0700

    ffix

--- lib/controllers/controller.dart
+++ lib/controllers/controller.dart
@@ -21,7 +21,6 @@ class FlowerController extends GetxController {
   RxBool isLoading = false.obs;
   final RxBool isLoggedIn = false.obs;
 
-  // Define a getter for the current user's email
   String get currentUserEmail {
     firebase_auth.User? user = firebase_auth.FirebaseAuth.instance.currentUser;
     return user?.email ?? 'N/A';
@@ -30,7 +29,6 @@ class FlowerController extends GetxController {
   Future<void> loginWithEmailAndPassword(String email, String password) async {
     try {
       if (email.trim().isEmpty || password.trim().isEmpty) {
-        // Display an error message if email or password is empty
         throw FirebaseAuthException(
           code: 'ERROR_INVALID_INPUT',
           message: 'Please enter both email and password.',
@@ -41,18 +39,11 @@ class FlowerController extends GetxController {
         email: email.trim(),
         password: password.trim(),
       );
-
-     // Navigate to the next screen or perform other actions after successful login
-      print('Email/Password Login Successful');
+      print('Email Login Successful');
     } on FirebaseAuthException catch (e) {
-      // Handle specific Firebase Auth exceptions
       print('Firebase Auth Exception: $e');
-      // You can display an error message to the user based on the exception code
-      // For example, check e.code and display an appropriate error message.
     } catch (e) {
-      // Handle other exceptions
       print('Error during Email/Password Login: $e');
-      // Display a generic error message to the user
     }
   }
 
@@ -127,7 +118,7 @@ class FlowerController extends GetxController {
   Future<void> logout() async {
     try {
       await _auth.signOut();
-      Get.offAllNamed('/login'); 
+      Get.offAllNamed('/login');
     } catch (error) {
       print('Error during logout: $error');
     }
