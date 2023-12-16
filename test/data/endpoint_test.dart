a649f2440fdac911813b6f72a6f335a3056c26a0
ekanurmairisma <ekanurmairisma@gmail.com>
2023-12-16 12:55:33 +0700

    up

--- test/data/endpoint_test.dart
+++ test/data/endpoint_test.dart
@@ -3,11 +3,12 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('ClientController initialization', () {
-    // Inisialisasi ClientController dan panggil onInit secara manual
+
     final clientController = ClientController();
     clientController.onInit();
 
-    // Pastikan nilai-nilai yang diatur sesuai dengan yang diharapkan
+    expect(clientController.client.endPoint, isNotEmpty);
+
     expect(clientController.client.endPoint, equals('https://cloud.appwrite.io/v1'));
 
   });
